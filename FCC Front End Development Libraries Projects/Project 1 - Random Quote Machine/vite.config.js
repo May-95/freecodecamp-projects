b619b4c999c50b7f6a63e6fdcb64c0a647afae20
Sumaiya A <107862616+May-95@users.noreply.github.com>
2023-11-12 23:04:36 +0000

    amend

--- FCC Front End Development Libraries Projects/Project 1 - Random Quote Machine/vite.config.js
+++ FCC Front End Development Libraries Projects/Project 1 - Random Quote Machine/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
-  base: "/freecodecamp-projects/FCC Front End Development Libraries Projects/Project 1 - Random Quote Machine/",
   server: {
     proxy: {
       "/api": {
